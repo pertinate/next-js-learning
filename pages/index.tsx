8fab28b18baaba908b647f0fb24b8d7c58a54018
Pertinate <ppertinate@gmail.com>
2021-01-09 21:26:40 -0600

    Initial set up of typescript + tailwind + next.js

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>My NEXT.JS App</title>
+            </Head>
+            <main
+                className='bg-gray-200 h-screen'
+            >
+                test
+            </main>
+        </div>
+    );
+}
